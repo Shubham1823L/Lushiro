95ad94847a500f992306b99a0398f70e14036ff5
Shubham1823L <shubhuisbetter@gmail.com>
2025-11-15 10:19:02 +0530

    docker dev+prod setup done

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose'
 import env from './env.js'
-import CustomError from '../utils/CustomError.js'
 
 async function connectDB() {
     try {
         await mongoose.connect(env.MONGO_URI)
         console.log("Connected to DB")
     } catch (error) {
-        throw new CustomError(500,"DB_ERROR","Connection to the Database could not be established")
+        console.error(500, "DB_ERROR", "Connection to the Database could not be established", error)
     }
 }
 
