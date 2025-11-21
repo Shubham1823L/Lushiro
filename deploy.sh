731f6d59298001128c4c1fe3d4d51490521471ca
Shubham1823L <shubhuisbetter@gmail.com>
2025-11-21 22:42:26 +0530

    CI/CD workflow try-12

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 cd /opt/myapp
 
 echo Pulling latest images...
-sudo docker -f compose.prod.yaml pull
+sudo docker compose -f compose.prod.yaml pull
 
 echo Building and then running...
-sudo docker -f compose.prod.yaml up --build
+sudo docker compose -f compose.prod.yaml up --build
 
 echo Cleaning up old images...
 sudo docker system prune -af --volumes
